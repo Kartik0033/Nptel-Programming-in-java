93ba990b5456d783f44a0f6cbe099ae156d90e0e
KartikGaikwad <kartikgaikwad9284868334@gmail.com>
2026-04-18 17:07:06 +0530

    Thread extends thread using class thread java lang

---
+++ Java_program/ThreadClass.java
@@ -0,0 +1,28 @@
+class A extends Thread{
+  public void run(){
+    int i ;
+    for(i=0 ; i<11 ; i++){
+      System.out.println("Thread A:"+i);
+    }
+  }
+}
+
+class B extends Thread{
+  public void run(){
+    int i ;
+    for(i=0 ; i<11; i++){
+      System.out.println("Thread B:"+i);
+    }
+  }
+}
+
+ public class ThreadClass {
+
+  public static void main(String []args){
+    A t1 = new A();
+    B t2 = new B();
+    t1.start();
+    t2.start();
+  }
+  
+}
