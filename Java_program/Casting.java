cc71100759fd4bed74cdb46324e3ecd1e00df75e
KartikGaikwad <kartikgaikwad9284868334@gmail.com>
2026-04-21 11:17:21 +0530

    Type casting exxample

---
+++ Java_program/Casting.java
@@ -0,0 +1,8 @@
+class Casting {
+  public static void  main (String []  newd){
+    System.out.println("Hello Good Morning");
+    for (int i = 0; i < newd.length ; i++) {
+      System.out.println(newd[i]);
+    }
+  }
+}
---
+++ Java_program/Question.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class Question {
+public static void main(String[] args)
+{
+try {
+PrintWriter writer = new PrintWriter(System.out);
+writer.write('4'+ '2');
+writer.print('4'+'2');
+writer.close();
+}
+catch (Exception e) {
+System.out.println(e);
+}
+}
+}
