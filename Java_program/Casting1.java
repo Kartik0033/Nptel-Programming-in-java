ed84eb05355c7874f4e7f2be3f793ad960be2302
KartikGaikwad <kartikgaikwad9284868334@gmail.com>
2026-04-21 11:34:10 +0530

    Casting1 exm

---
+++ Java_program/Casting1.java
@@ -0,0 +1,12 @@
+public class Casting1 {
+  public static void main(String[] args) {
+      System.out.println('7'+'1'+"1st");
+      System.out.write(97);
+      char c='A'+1;
+      int a = 'a';
+      System.out.println(a);
+
+      System.out.println(c);
+      System.out.flush();
+  }
+}
